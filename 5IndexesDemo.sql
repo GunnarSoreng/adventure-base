6ea5619b8b67b5a5d56ccbd742976703a700631a
Gunnar Søreng <gunnar.soreng@nhn.no>
2017-03-08 15:17:14 +0100

    Added testing of indexes, optimalization, XML , JSON, grouping and nested selects

--- 5IndexesDemo.sql
+++ 5IndexesDemo.sql
@@ -21,3 +21,131 @@ into dbo.SalesOrderDetail
 from Sales.SalesOrderDetail;
 
 select * from dbo.SalesOrderDetail;
+
+-- Set stats on so we can see how many pages are read
+SET STATISTICS IO ON;
+
+-- Show the actual execution plan
+
+-- Get products and quantities for a specific order (no index)
+SELECT ProductID, OrderQty
+FROM dbo.SalesOrderDetail
+WHERE SalesOrderID = 58125;
+-- Logical reads: 1494
+
+-- Create a clustered index
+CREATE CLUSTERED INDEX idx_SalesOrderID
+ON dbo.SalesOrderDetail(SalesOrderID);
+
+-- Try the query again with index
+SELECT ProductID, OrderQty
+FROM dbo.SalesOrderDetail
+WHERE SalesOrderID = 58125;
+-- Logical reads: 3
+-- Great savings using the index!
+
+
+-- Get orders by product (uses the previously created clustered index, but not using it to search)
+SELECT SalesOrderID
+FROM dbo.SalesOrderDetail
+WHERE ProductID = 758;
+-- Logical reads: 1581
+
+-- A Table can only have ONE clustered index,data can only be stored in one order.
+
+-- Create nonclustered index
+CREATE NONCLUSTERED INDEX idx_ProductID
+ON dbo.SalesOrderDetail(ProductID);
+
+-- Try the query again (using the nonclustered index)
+SELECT SalesOrderID
+FROM dbo.SalesOrderDetail
+WHERE ProductID = 758;
+-- Logical reads: 2
+
+-- Include a non-indexed field
+SELECT SalesOrderID, OrderQty
+FROM dbo.SalesOrderDetail
+WHERE ProductID = 758;
+-- Logical reads: 828
+
+
+--
+--  COLUMNSTORE Indexes
+--
+-- Are stored in memory
+-- Store data by column instead of by row
+-- Use Compression to optimize memory usage and performance
+
+-- Can be clustered or non-clustered
+-- Clustered columnstore indexes include all columns
+-- Only one clustered columnstore index per table
+
+
+-- Need to create test tables
+SELECT * from Production.Product;
+
+SELECT * FROM Production.ProductInventory;
+
+SELECT * 
+INTO dbo.Product
+from Production.Product;
+
+SELECT * 
+INTO dbo.ProductInventory
+FROM Production.ProductInventory;
+
+-- Query tables
+SELECT * FROM dbo.Product;
+SELECT * FROM dbo.ProductInventory;
+
+-- Set stats on ( if not already done )
+--SET STATISTICS IO ON;
+
+-- Show actual execution plan (select button 'include Actual Execution Plan' in toolbar, <ctrl>+M )
+
+-- Query tables
+SELECT p.Name, p.ListPrice, i.Shelf, i.Bin, i.Quantity
+FROM dbo.Product as p
+JOIN dbo.ProductInventory AS i
+ON p.ProductID = i.ProductID
+WHERE p.Name = 'Rear Brakes';
+-- Logical reads: ProductInventory -> 7, Product -> 13
+
+
+-- Create columnstore index 
+-- (are always created on the whole table)
+CREATE CLUSTERED COLUMNSTORE INDEX idx_ProductInventory
+on dbo.ProductInventory;
+-- Output: "Msg 35338, Level 16, State 1, Line 1. Clustered columnstore index is not supported."
+
+-- Stackexchange: "SQL Server 2012 only supports the creation of nonclustered indexes"!!
+--
+-- Use: CREATE NONCLUSTERED COLUMNSTORE INDEX
+-- In SQL Server 2012 this will make your table read-only. You should probably look at 
+-- adding all three columns of the table to the columnstore index but I'll leave that to your testing.
+-- Note that columnstore indexes also require Enterprise Edition.
+-- Using this instead, just to test it:
+CREATE NONCLUSTERED COLUMNSTORE INDEX idx_ProductInventory
+on dbo.ProductInventory(ProductID, Shelf, Bin, Quantity);
+
+
+-- Query tables
+SELECT p.Name, p.ListPrice, i.Shelf, i.Bin, i.Quantity
+FROM dbo.Product as p
+JOIN dbo.ProductInventory AS i
+ON p.ProductID = i.ProductID
+WHERE p.Name = 'Rear Brakes';
+
+-- Create NonClustered ColumnStore index
+CREATE NONCLUSTERED COLUMNSTORE INDEX idx_Product
+ON dbo.Product (ProductID, Name, ListPrice);
+
+-- Query tables
+SELECT p.Name, p.ListPrice, i.Shelf, i.Bin, i.Quantity
+FROM dbo.Product as p
+JOIN dbo.ProductInventory AS i
+ON p.ProductID = i.ProductID
+WHERE p.Name = 'Rear Brakes';
+
+
--- Testing AdventureWorks 2012 database.sql
+++ Testing AdventureWorks 2012 database.sql
@@ -48,6 +48,8 @@ where  pp.BusinessEntityID = ppp.BusinessEntityID and
 
 select * from vw_NameInfo;
 
+select top 50 * from vw_NameInfo;
+
 -- Pseudocode Examples from presentation:
 
 --update savings 
@@ -73,3 +75,107 @@ select * from vw_NameInfo;
 --commit transaction
 -- or if some errors occur:
 --rollback transaction
+
+
+-- Testing count and group by:
+
+select FirstName, LastName
+from Person.Person
+where LastName IN ('Carreras','Gee','Harris','Gates','Harrington');
+
+
+select LastName "Last Name", count(LastName) "Number"
+from Person.Person
+where LastName IN ('Carreras','Gee','Harris','Gates','Harrington')
+group by LastName
+order by 2 desc;
+
+
+select LastName "Last Name", count(LastName) "Number"
+from Person.Person
+group by LastName
+order by 2 desc;
+
+select top 25 LastName "Last Name", count(LastName) "Number"
+from Person.Person
+group by LastName
+order by 2 desc;
+
+select FirstName, LastName
+from Person.Person
+where LastName = 'Diaz'
+order by 1;
+
+
+-- Testing nested selects
+Select personName.FullName "Name", COUNT(personName.FullName) "Number"
+FROM 
+	(select FirstName + ' ' + LastName as Fullname
+	 from Person.Person
+	 where LastName = 'Diaz') personName
+Group by personName.Fullname
+order by 2 desc, 1 asc
+;
+
+
+--
+--     XML
+--
+
+-- Generate XML from query
+Select top 10 personName.FullName "Name", COUNT(personName.FullName) "Number"
+FROM 
+	(select FirstName + ' ' + LastName as Fullname
+	 from Person.Person
+	 where LastName = 'Diaz') personName
+Group by personName.Fullname
+order by 2 desc, 1 asc
+for XML AUTO, ROOT('Person');
+;
+
+-- Read in XML from a variable 'idoc'
+-- start assigning xml objects to new columns after traversing from ROOT to Customer node
+SELECT * 
+FROM OPENXML (@idoc, 'ROOT/Customer',1)
+WITH (CustomerID varchar(10), ContactName varchar(20));
+
+
+
+--
+--    JSON
+--
+
+-- Create JSON from query
+-- Only works with SQL Server 2016 or later!!!
+--
+Select top 10 personName.FullName "Name", COUNT(personName.FullName) "Number"
+FROM 
+	(select FirstName + ' ' + LastName as Fullname
+	 from Person.Person
+	 where LastName = 'Diaz') personName
+Group by personName.Fullname
+order by 2 desc, 1 asc
+for JSON AUTO;
+
+
+select FirstName, LastName
+from Person.Person
+where LastName = 'Diaz'
+FOR JSON AUTO;
+
+
+-- In PATH mode, you can use the dot syntax – for example, 'Item.Price' – to format nested output. 
+-- Use the ROOT option to specify a named root element.
+SELECT TOP 20
+   SalesOrderNumber AS 'Order.Number',
+   OrderDate as 'Order.Date'
+FROM Sales.SalesOrderHeader
+--FOR JSON PATH
+;
+
+-- Read JSON from variable @json
+declare @json nvarchar(255)
+set @json =
+N'[ null, "string", 1, true]';
+select * FROM OPENJSON( @json );
+
